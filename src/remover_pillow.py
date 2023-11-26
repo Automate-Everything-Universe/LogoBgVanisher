b50b645c90843771b43373fdb5872c6b28c8c105
dragosjosan <Dragosjosan@gmail.com>
2023-11-26 15:28:07 +0100

    Refactor remover to avoid DRY regarding saving the image and processing

--- src/remover_pillow.py
+++ src/remover_pillow.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from PIL import Image, ImageFilter
 
 from background_remover import BackgroundRemovalStrategy
+from folder_utils import save_image
 
 
 class PillowBackgroundRemoval(BackgroundRemovalStrategy):
@@ -18,16 +19,18 @@ class PillowBackgroundRemoval(BackgroundRemovalStrategy):
     def __init__(self, tolerance: int = 50, edge_tolerance: int = 50):
         self.tolerance = tolerance
         self.edge_tolerance = edge_tolerance
+        self.suffix = "converted_pillow"
 
     def remove_background(self, input_path: Path, output_path: Union[bool, Path] = False) -> None:
         img = Image.open(input_path).convert("RGBA")
+        processed_img = self._process_image_data(img=img)
+        self._save_image(img=img, input_path=input_path, output_path=output_path, suffix=self.suffix)
 
+    def _process_image_data(self, img: Image) -> list:
         # Get the background color (assuming it's the color of the top-left pixel)
         bg_color = img.getpixel((0, 0))
-
         # Convert img to grayscale for better edge detection and create an edge mask
         edge_mask = img.convert("L").filter(ImageFilter.FIND_EDGES)
-
         data = img.getdata()
         edge_data = edge_mask.getdata()
         new_data = []
@@ -43,12 +46,8 @@ class PillowBackgroundRemoval(BackgroundRemovalStrategy):
                 else:
                     new_data.append(item)
         img.putdata(new_data)
-        if not output_path:
-            output_path = Path(f"{input_path.stem}_Pillow_converted.png")
-        else:
-            if not output_path.is_dir():
-                output_path.mkdir(parents=True, exist_ok=True)
-                output_path = output_path / f"{input_path.stem}_Pillow_converted.png"
-            else:
-                output_path = output_path / f"{input_path.stem}_Pillow_converted.png"
-        img.save(output_path, "PNG")
+        return img
+
+    @staticmethod
+    def _save_image(img: Image, input_path: Path, output_path: Union[bool, Path], suffix: str):
+        save_image(img=img, input_path=input_path, output_path=output_path, suffix=suffix)
