8312e65d0cad7d262c04acd2b33ab0178b1e2e8e
dragosjosan <Dragosjosan@gmail.com>
2023-11-29 19:32:59 +0100

    Add test for Rmbg

--- tests/test_transparent.py
+++ tests/test_transparent.py
@@ -3,6 +3,7 @@ import pytest
 from pathlib import Path
 from PIL import Image
 
+from src import RmbgrBackgroundRemoval
 from src.remover_pillow import PillowBackgroundRemoval
 
 TEST_FOLDER = Path(__file__).parents[0]
@@ -23,8 +24,24 @@ def output_filename() -> Path:
     return TEST_FOLDER / "logo/logo_pillow_converted.png"
 
 
-def test_background_removal(input_path, output_path, output_filename):
+def test_transparent_pillow(input_path, output_path, output_filename):
     remover = PillowBackgroundRemoval()
+    remover.remove_background(input_path=input_path, output_path=output_path)
+    # Test that the processed image was created
+    assert output_filename.exists(), "Processed image file does not exist"
+
+    # Open the processed image and check for transparency
+    with Image.open(output_filename) as processed_img:
+        assert processed_img.mode == 'RGBA', "Image is not in RGBA mode"
+        corner_pixel = processed_img.getpixel((0, 0))
+        assert corner_pixel[3] == 0, "Background is not transparent"
+    # Clean up
+
+    os.remove(output_filename)
+
+
+def test_removal_rmbg(input_path, output_path, output_filename):
+    remover = RmbgrBackgroundRemoval()
     remover.remove_background(input_path=input_path, output_path=output_path)
     # Test that the processed image was created
     assert output_filename.exists(), "Processed image file does not exist"
