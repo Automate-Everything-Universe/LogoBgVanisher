7aa5d4893e1bf6faf5867caff07bea50e1186a2c
dragosjosan <Dragosjosan@gmail.com>
2023-12-04 10:53:07 +0100

    Add filename to rmbgr class

--- src/remover_rmbgr.py
+++ src/remover_rmbgr.py
@@ -10,10 +10,14 @@ from .background_remover import BackgroundRemovalStrategy
 class RmbgrBackgroundRemoval(BackgroundRemovalStrategy):
     def __init__(self, img):
         super().__init__(img)
-        self.image = img
+        self.filename = img.filename
         self.suffix = "rmbgr_converted"
 
     def remove_background(self) -> Image:
+        if self.image is None or not hasattr(self.image, 'convert'):
+            raise ValueError("Invalid image object provided")
+
         img = self.image.convert("RGBA")
         processed_img = rembg.remove(img)
+        processed_img.filename = self.filename
         return processed_img
