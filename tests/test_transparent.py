77692f55d93f2b527c622151129fcdfc8ec178f9
dragosjosan <Dragosjosan@gmail.com>
2023-12-04 10:53:31 +0100

    Adapt tests to work with new refactored classes

--- tests/test_transparent.py
+++ tests/test_transparent.py
@@ -3,59 +3,67 @@ import pytest
 from pathlib import Path
 from PIL import Image
 
-from src import RmbgrBackgroundRemoval
+from src.folder_utils import load_image
+from src.remover_rmbgr import RmbgrBackgroundRemoval
 from src.remover_pillow import PillowBackgroundRemoval
+from src.saver import SavePic
 
 TEST_FOLDER = Path(__file__).parents[0]
 
 
 @pytest.fixture
-def input_path() -> Path:
+def file() -> Path:
     return TEST_FOLDER / "logo/logo.png"
 
 
 @pytest.fixture
-def output_path() -> Path:
+def input_path() -> Path:
     return TEST_FOLDER / "logo/"
 
 
 @pytest.fixture
-def output_filename_pillow() -> Path:
-    return TEST_FOLDER / "logo/logo_pillow_converted.png"
+def expected_pillow() -> Path:
+    return TEST_FOLDER / "logo/logo_PIL_converted.png"
 
 
 @pytest.fixture
-def output_filename_rmbg() -> Path:
+def expected_rmbg() -> Path:
     return TEST_FOLDER / "logo/logo_rmbgr_converted.png"
 
 
-def test_transparent_pillow(input_path, output_path, output_filename_pillow):
-    remover = PillowBackgroundRemoval()
-    remover.remove_background(input_path=input_path, output_path=output_path)
-    # Test that the processed image was created
-    assert output_filename_pillow.exists(), "Processed image file does not exist"
+def test_transparent_pillow(file, expected_pillow):
+    image_object = load_image(picture=file)
+    remover = PillowBackgroundRemoval(img=image_object)
+    image_object = remover.remove_background()
+    image_saver = SavePic(img=image_object)
+    image_saver.save_image(suffix="PIL_converted")
+
+    # Check if processed image was created
+    assert expected_pillow.exists(), "Processed image file does not exist"
 
     # Open the processed image and check for transparency
-    with Image.open(output_filename_pillow) as processed_img:
-        assert processed_img.mode == 'RGBA', "Image is not in RGBA mode"
-        corner_pixel = processed_img.getpixel((0, 0))
-        assert corner_pixel[3] == 0, "Background is not transparent"
+    assert image_object.mode == 'RGBA', "Image is not in RGBA mode"
+    corner_pixel = image_object.getpixel((0, 0))
+    assert corner_pixel[3] == 0, "Background is not transparent"
+
     # Clean up
+    os.remove(expected_pillow)
 
-    os.remove(output_filename_pillow)
 
+def test_removal_rmbg(file, expected_rmbg):
+    image_object = load_image(picture=file)
+    remover = RmbgrBackgroundRemoval(img=image_object)
+    image_object = remover.remove_background()
 
-def test_removal_rmbg(input_path, output_path, output_filename_rmbg):
-    remover = RmbgrBackgroundRemoval()
-    remover.remove_background(input_path=input_path, output_path=output_path)
+    image_saver = SavePic(img=image_object)
+    image_saver.save_image(suffix="rmbgr_converted")
     # Test that the processed image was created
-    assert output_filename_rmbg.exists(), "Processed image file does not exist"
+    assert expected_rmbg.exists(), "Processed image file does not exist"
 
     # Open the processed image and check for transparency
-    with Image.open(output_filename_rmbg) as processed_img:
-        assert processed_img.mode == 'RGBA', "Image is not in RGBA mode"
-        corner_pixel = processed_img.getpixel((0, 0))
-        assert corner_pixel[3] == 0, "Background is not transparent"
-    # Clean up
+    assert image_object.mode == 'RGBA', "Image is not in RGBA mode"
+    corner_pixel = image_object.getpixel((0, 0))
+    assert corner_pixel[3] == 0, "Background is not transparent"
 
-    os.remove(output_filename_rmbg)
+    # Clean up
+    os.remove(expected_rmbg)
