aaf72492ea643b146e7440f5c4d529a68f7d0633
dragosjosan <Dragosjosan@gmail.com>
2023-12-12 14:39:36 +0100

    Adapt method to return int instead of tuple if only one user argument is given

--- src/logo_bg_vanisher/utils_validation.py
+++ src/logo_bg_vanisher/utils_validation.py
@@ -1,7 +1,7 @@
 """
 Module to handle user validation
 """
-from typing import Tuple
+from typing import Tuple, Union
 
 
 def is_string_valid(text: str) -> str:
@@ -15,7 +15,7 @@ def is_string_valid(text: str) -> str:
     return text
 
 
-def are_numbers_valid(*args) -> Tuple[int, ...]:
+def are_numbers_valid(*args) -> Union[Tuple[int, ...], int]:
     """
     Validates that all provided arguments are integers.
 
@@ -27,8 +27,12 @@ def are_numbers_valid(*args) -> Tuple[int, ...]:
         raise ValueError("No arguments provided!")
 
     items = []
-    for item in args:
-        if not isinstance(item, int):
-            raise ValueError(f"Wrong type {type(item)}. Expected int")
-        items.append(item)
-    return tuple(items)
+    try:
+        if len(args) == 1:
+            return int(args[0])
+        for item in args:
+            item = int(item)
+            items.append(item)
+        return tuple(items)
+    except ValueError:
+        raise ValueError("Argument needs to be an integer")
